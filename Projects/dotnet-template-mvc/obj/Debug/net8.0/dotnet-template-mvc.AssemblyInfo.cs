73717aeccc9aeae774b53eab388e816e6c59836b
primetimetran <primetimetran@gmail.com>
2024-04-17 15:18:48 -0400

    Update

--- Projects/dotnet-template-mvc/obj/Debug/net8.0/dotnet-template-mvc.AssemblyInfo.cs
+++ Projects/dotnet-template-mvc/obj/Debug/net8.0/dotnet-template-mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-template-mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c653bd2f838a937a5132761f990f56d7ebeb2397")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18dc39ec0e423fa8bcdf60360bfcf7a1eac87bc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-template-mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-template-mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
